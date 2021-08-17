3683f8f2d9e291b548c9c8a8727dc11a8fb944ff
Kminutti <58534537+K-minutti@users.noreply.github.com>
2021-08-17 16:17:05 +0000

    Add middleware Delete last build files

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, {Application, Request, Response, NextFunction} from "express";
 
-const app = express();
+const app: Application = express();
 
+//Body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 export default app
