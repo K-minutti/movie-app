43985a0e9c6fa34bb26888884e0e9ce2aed2561b
Kevin <58534537+K-minutti@users.noreply.github.com>
2021-08-24 20:01:16 +0000

    Serving static files

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, {Application, Request, Response, NextFunction} from "express";
+import path from "path";
 
 const app: Application = express();
 
 //Body parsing middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 
+//Static files
+app.use(express.static(path.join(__dirname, "../public/")));
 
 
 export default app
